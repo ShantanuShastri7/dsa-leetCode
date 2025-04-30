1fbe1bebe9ea209488edfb9bcac5391962b3b8de
ShantanuShastri7 <69163724+ShantanuShastri7@users.noreply.github.com>
2025-04-30 21:50:08 +0530

    Time: 3 ms (89.86%) | Memory: 43.4 MB (81.09%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -2,17 +2,17 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] result = new int[nums1.length];
         Map<Integer, Integer> nums2NextGreaterMap = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for(int i=0; i<nums2.length; i++){
-            boolean nextGreaterFound = false;
-            for(int j=i+1; j<nums2.length; j++){
-                if(nums2[j]>nums2[i]){
-                    nextGreaterFound=true;
-                    nums2NextGreaterMap.put(nums2[i], nums2[j]);
-                    break;
-                }
+        for(int num : nums2){
+            while(!stack.isEmpty() && num>stack.peek()) {
+                nums2NextGreaterMap.put(stack.pop(), num);
             }
-            if(!nextGreaterFound) nums2NextGreaterMap.put(nums2[i], -1);
+            stack.push(num);
+        }
+
+        while(!stack.isEmpty()){
+            nums2NextGreaterMap.put(stack.pop(), -1);
         }
 
         for(int i=0; i<nums1.length; i++){
