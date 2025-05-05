026ccfbc906e065e613fc963bbdb330d05419d78
ShantanuShastri7 <69163724+ShantanuShastri7@users.noreply.github.com>
2025-05-05 22:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (70.13%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTilings(int n) {
+        long divisor = (long)Math.pow(10,9) +7;
+
+        if (n==0) return 1;
+        else if(n==1) return 1;
+        else if(n==2) return 2;
+
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            dp[i]=(2*dp[i-1]%divisor + dp[i-3])%divisor;
+        }
+        return (int)dp[n];
+    }
+}
