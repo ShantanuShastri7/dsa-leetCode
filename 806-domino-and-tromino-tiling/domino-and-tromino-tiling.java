cb5b7cd3071d83209d4e1e8893bf7032dc2d1026
ShantanuShastri7 <69163724+ShantanuShastri7@users.noreply.github.com>
2025-05-05 22:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (39.16%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -12,7 +12,7 @@ class Solution {
         dp[2] = 2;
 
         for(int i=3; i<=n; i++){
-            dp[i]=(2*dp[i-1]%divisor + dp[i-3])%divisor;
+            dp[i]=(2*dp[i-1] + dp[i-3])%divisor;
         }
         return (int)dp[n];
     }
