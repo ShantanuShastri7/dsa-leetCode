55379747deafbd2ad52f5a0af9d8e2304f0cbc99
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-05-24 19:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.67%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,13 +1,15 @@
 class Solution {
     public int numTrees(int n) {
-        if (n <= 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        int total = 0;
-        for (int i = 1; i <= n; i++) {
-            int left = numTrees(i - 1);
-            int right = numTrees(n - i);
-            total += left * right;
+        for(int i=2; i<=n; i++){
+            for(int j=1;j<=i; j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
         }
-        return total;
+
+        return dp[n];
     }
 }
