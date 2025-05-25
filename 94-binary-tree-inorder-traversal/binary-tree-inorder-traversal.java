90337044ba18289579fd3f5ef7669c581c49965b
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-05-25 14:16:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (27.87%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -13,26 +13,50 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if(root == null){
+//             return new ArrayList<>();
+//         }
+
+//         List<Integer> results = new ArrayList<>();
+//         results = recurseInorder(root, results);
+//         return results;
+//     }
+
+//     private List<Integer> recurseInorder(TreeNode root, List<Integer> result){
+//         if(root == null){
+//             return result;
+//         } 
+
+//         recurseInorder(root.left, result);
+//         result.add(root.val);
+//         recurseInorder(root.right, result);
+
+//         return result;
+//     }
+// }
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         if(root == null){
             return new ArrayList<>();
         }
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        TreeNode curr=root;
 
-        List<Integer> results = new ArrayList<>();
-        results = recurseInorder(root, results);
-        return results;
-    }
+        while(!stack.isEmpty()){
+            while(curr!=null){
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr=curr.right;
+        }
 
-    private List<Integer> recurseInorder(TreeNode root, List<Integer> result){
-        if(root == null){
-            return result;
-        } 
-
-        recurseInorder(root.left, result);
-        result.add(root.val);
-        recurseInorder(root.right, result);
-
-        return result;
+        return result.subList(0,result.size()-1);
     }
 }
