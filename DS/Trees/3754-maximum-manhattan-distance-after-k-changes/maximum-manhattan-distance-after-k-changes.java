d16dee5faaff6df9cbf563a5f6d760be91ac7349
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-06-26 08:24:49 +0530

    Time: 141 ms (13.74%) | Memory: 45.7 MB (67.91%) - LeetSync

---
+++ DS/Trees/3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxDistance(String s, int k) {
+        return Math.max(Math.max(helper(s,'N','E',k),helper(s,'N','W',k)),Math.max(helper(s,'S','E',k),helper(s,
+                    'S','W',k)));
+    }
+
+    private int helper (String s, char a, char b, int k){
+        int pos = 0, used = 0, maxDist = 0;
+        for (char c : s.toCharArray()) {
+            if (c == a || c == b) {
+                pos++;
+            } else if (used < k) {
+                pos--;
+                used++;
+            } else {
+                pos--;
+            }
+            maxDist = Math.max(maxDist, pos + 2 * Math.min(k, used));
+        }
+        return maxDist;
+    }
+}
