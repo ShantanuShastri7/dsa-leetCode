8f6ea2a5dd3e9fceaa883f2630d9e7706f09933e
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-06-28 18:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (8.55%) - LeetSync

--- DS/DP/198-house-robber/house-robber.java
+++ DS/DP/198-house-robber/house-robber.java
@@ -1,21 +1,43 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] memo = new int[nums.length];
-        memo[0]=nums[0];
+        int prev = nums[0];
+        int prev2 = 0;
 
         for(int i=1; i<nums.length; i++){
             int pick = nums[i];
-            if(i>1) pick+=memo[i-2];
+            if(i>1) pick+=prev2;
 
-            int notPick = memo[i-1];
+            int notPick = prev;
 
-            memo[i] = Math.max(pick, notPick);
+            int curr = Math.max(pick, notPick);
+            prev2=prev;
+            prev=curr;
         }
         
-        return memo[nums.length-1];
-        // Arrays.fill(memo, -1);
-        // return helper(nums.length-1, nums, memo);
+        return prev;
     }
+
+    // public int rob(int[] nums) {
+    //     int[] memo = new int[nums.length];
+    //     memo[0]=nums[0];
+
+    //     for(int i=1; i<nums.length; i++){
+    //         int pick = nums[i];
+    //         if(i>1) pick+=memo[i-2];
+
+    //         int notPick = memo[i-1];
+
+    //         memo[i] = Math.max(pick, notPick);
+    //     }
+        
+    //     return memo[nums.length-1];
+    // }
+
+    // public int rob(int[] nums) {
+    //     int[] memo = new int[nums.length];
+    //     // Arrays.fill(memo, -1);
+    //     // return helper(nums.length-1, nums, memo);
+    // }
 
     private int helper(int index, int[] nums, int[] memo){
         if(index==0) return nums[0];
