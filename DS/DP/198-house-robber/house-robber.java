fb370720b70ba7fd6c1858bb48546d0735145430
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-06-28 17:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (88.46%) - LeetSync

--- DS/DP/198-house-robber/house-robber.java
+++ DS/DP/198-house-robber/house-robber.java
@@ -1,9 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
         int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
+        memo[0]=nums[0];
 
-        return helper(nums.length-1, nums, memo);
+        for(int i=1; i<nums.length; i++){
+            int pick = nums[i];
+            if(i>1) pick+=memo[i-2];
+
+            int notPick = memo[i-1];
+
+            memo[i] = Math.max(pick, notPick);
+        }
+        
+        return memo[nums.length-1];
+        // Arrays.fill(memo, -1);
+        // return helper(nums.length-1, nums, memo);
     }
 
     private int helper(int index, int[] nums, int[] memo){
