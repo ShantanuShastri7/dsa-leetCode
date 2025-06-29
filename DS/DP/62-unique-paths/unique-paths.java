aa213e70aa95124872f77f629380004f25350881
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-06-29 17:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (76.21%) - LeetSync

---
+++ DS/DP/62-unique-paths/unique-paths.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0&&j==0) dp[i][j]=1;
+                else{
+                    int up=0; int left=0;
+                    if(i!=0) up = dp[i-1][j];
+                    if(j!=0) left = dp[i][j-1];
+                    dp[i][j] = left+up;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    // public int uniquePaths(int m, int n) {
+    //     int memo[][] = new int[m+1][n+1];
+    //     for(int i=0;i<m+1;i++){
+    //         Arrays.fill(memo[i],-1);
+    //     }
+    //     return helper(m,n,memo);
+    // }
+
+    private int helper(int m, int n, int memo[][]){
+        if(m==1 && n==1) return 1;
+        else if(m<1||n<1) return 0;
+        if(memo[m][n]!=-1) return memo[m][n];
+
+        int up = uniquePaths(m-1,n);
+        int left = uniquePaths(m,n-1);
+        
+        return memo[m][n]=up+left;
+    }
+}
