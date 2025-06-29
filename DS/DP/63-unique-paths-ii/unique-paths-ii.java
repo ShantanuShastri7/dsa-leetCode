768c95e308355d660cc15d05de29ee953f49dc01
Shantanu Shastri <69163724+ShantanuShastri7@users.noreply.github.com>
2025-06-29 21:05:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (80.66%) - LeetSync

--- DS/DP/63-unique-paths-ii/unique-paths-ii.java
+++ DS/DP/63-unique-paths-ii/unique-paths-ii.java
@@ -1,11 +1,35 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] memo = new int[m][n];
-        for (int[] row : memo) Arrays.fill(row, -1);
-        return helper(m - 1, n - 1, memo, obstacleGrid);
+    int m = obstacleGrid.length;
+    int n = obstacleGrid[0].length;
+
+    int[] dp = new int[n];
+    dp[0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+
+    for (int i = 0; i < m; i++) {
+        int[] temp = new int[n];
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[i][j] == 1) {
+                temp[j] = 0;
+            } else {
+                int up = (i > 0) ? dp[j] : 0;
+                int left = (j > 0) ? temp[j - 1] : 0;
+                temp[j] = (i == 0 && j == 0) ? dp[0] : up + left;
+            }
+        }
+        dp = temp;
     }
+
+    return dp[n - 1];
+}
+
+    // public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    //     int m = obstacleGrid.length;
+    //     int n = obstacleGrid[0].length;
+    //     int[][] memo = new int[m][n];
+    //     for (int[] row : memo) Arrays.fill(row, -1);
+    //     return helper(m - 1, n - 1, memo, obstacleGrid);
+    // }
 
     private int helper(int m, int n, int[][] memo, int[][] obstacle) {
         if (m < 0 || n < 0) return 0;
