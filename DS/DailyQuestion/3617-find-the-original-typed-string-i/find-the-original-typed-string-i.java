5f6e0f7ba86b043333f86bf862a09159d1ed0bd3
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-01 20:36:50 +0530

    Time: 29 ms (1.65%) | Memory: 44.2 MB (7.44%) - LeetSync

---
+++ DS/DailyQuestion/3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        if(word==null) return 0;
+        if(word.length()<2) return 1;
+        int ans = 1;
+
+        for(int i=0; i<word.length(); i++){ 
+            for(int j=i+1; j<word.length(); j++){
+                System.out.print("i: "+i+" j: "+j+" ");
+                if(word.charAt(i)==word.charAt(j)){
+                    ans++;
+                } else{
+                    i=j-1;
+                    break;
+                }
+                if(j==word.length()-1) return ans;
+            }
+        }
+
+        return ans;
+    }
+}
