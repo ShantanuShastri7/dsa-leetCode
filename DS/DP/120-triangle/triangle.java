dd268e2fac0d8d5246edeb4fda3201cf8d810269
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-01 22:35:24 +0530

    Time: 6 ms (19.50%) | Memory: 45.9 MB (5.00%) - LeetSync

--- DS/DP/120-triangle/triangle.java
+++ DS/DP/120-triangle/triangle.java
@@ -1,23 +1,50 @@
 class Solution {
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] memo = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int i=0; i<memo[0].length; i++){
-            Arrays.fill(memo[i], 10000000);
+        int n = triangle.size();
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = triangle.get(0).get(0);
+
+        for (int i = 1; i < n; i++) {
+            int[] temp = new int[n];
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int val = triangle.get(i).get(j);
+                if (j == 0) {
+                    temp[j] = dp[j] + val;
+                } else if (j == triangle.get(i).size() - 1) {
+                    temp[j] = dp[j - 1] + val;
+                } else {
+                    temp[j] = Math.min(dp[j - 1], dp[j]) + val;
+                }
+            }
+            dp = temp;
         }
-        return helper(triangle, 0, 0, memo);
+
+        return Arrays.stream(dp).min().getAsInt();
     }
 
-    private int helper(List<List<Integer>> triangle, int level, int index, int[][] memo){
-        if(level==triangle.size()-1) {
+    // public int minimumTotal(List<List<Integer>> triangle) {
+    //     int[][] memo = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+    //     for(int i=0; i<memo[0].length; i++){
+    //         Arrays.fill(memo[i], 10000000);
+    //     }
+    //     return helper(triangle, 0, 0, memo);
+    // }
+
+    private int helper(List<List<Integer>> triangle, int level, int index, int[][] memo) {
+        if (level == triangle.size() - 1) {
             return triangle.get(level).get(index);
-        } 
-        if(level>triangle.size()-1 || index>triangle.get(level).size()-1) return 1000000;
-        if(memo[level][index]!=10000000) return memo[level][index];
+        }
+        if (level > triangle.size() - 1 || index > triangle.get(level).size() - 1)
+            return 1000000;
+        if (memo[level][index] != 10000000)
+            return memo[level][index];
 
-        int minSum=0;
-        int below = triangle.get(level).get(index) + helper(triangle, level+1, index, memo);
-        int belowAhead = triangle.get(level).get(index) + helper(triangle, level+1, index+1, memo);
+        int minSum = 0;
+        int below = triangle.get(level).get(index) + helper(triangle, level + 1, index, memo);
+        int belowAhead = triangle.get(level).get(index) + helper(triangle, level + 1, index + 1, memo);
 
-        return memo[level][index]=Math.min(below, belowAhead);
+        return memo[level][index] = Math.min(below, belowAhead);
     }
 }
