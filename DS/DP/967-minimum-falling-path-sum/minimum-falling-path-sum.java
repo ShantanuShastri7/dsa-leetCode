c841bd64ae785a2184184802c6e365edd453f400
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-02 10:34:08 +0530

    Time: 6 ms (71.80%) | Memory: 45.8 MB (12.29%) - LeetSync

--- DS/DP/967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ DS/DP/967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,16 +1,39 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int res = 100000000;
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix[0].length; i++) {
-            Arrays.fill(dp[i], 100000000);
-        }
-        for (int i = 0; i < matrix[0].length; i++) {
-            res = Math.min(res, helper(matrix, matrix.length - 1, i, dp));
+        int[] dp = new int[matrix[0].length];
+        int n = matrix.length;
+        if(n==1) return matrix[0][0];
+        Arrays.fill(dp, 10000000);
+
+        for (int i = 0; i < n; i++) {
+            int[] temp = new int[matrix[0].length];
+            for (int j = 0; j < n; j++) {
+                if(j==0){
+                    temp[j] = matrix[i][j] + Math.min(dp[j], dp[j+1]);
+                } else if(j==n-1){
+                    temp[j] = matrix[i][j] + Math.min(dp[j], dp[j-1]);
+                } else {
+                    temp[j] = matrix[i][j] + Math.min(Math.min(dp[j], dp[j+1]), dp[j-1]);
+                }   
+            }
+            dp=temp;
         }
 
-        return res;
+        return Arrays.stream(dp).min().getAsInt()-10000000;
     }
+
+    // public int minFallingPathSum(int[][] matrix) {
+    //     int res = 100000000;
+    //     int[][] dp = new int[matrix.length][matrix[0].length];
+    //     for (int i = 0; i < matrix[0].length; i++) {
+    //         Arrays.fill(dp[i], 100000000);
+    //     }
+    //     for (int i = 0; i < matrix[0].length; i++) {
+    //         res = Math.min(res, helper(matrix, matrix.length - 1, i, dp));
+    //     }
+
+    //     return res;
+    // }
 
     private int helper(int[][] matrix, int m, int n, int[][] dp) {
         if (m == 0)
