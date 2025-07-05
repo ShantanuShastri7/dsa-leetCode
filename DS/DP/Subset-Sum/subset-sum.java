fe5ddb7dd1b5664c81e68066e3328c1d3fd1f012
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-05 10:14:45 +0530

    Update subset-sum.java

--- DS/DP/Subset-Sum/subset-sum.java
+++ DS/DP/Subset-Sum/subset-sum.java
@@ -1,3 +1,32 @@
+//tabulation 
+class Solution {
+    public Boolean isSubsetSum(int arr[], int sum) {
+        int n = arr.length;
+        boolean[][] dp = new boolean[n][sum + 1];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        if (arr[0] <= sum) {
+            dp[0][arr[0]] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int target = 1; target <= sum; target++) {
+                boolean notPick = dp[i - 1][target];
+                boolean pick = false;
+                if (target >= arr[i]) {
+                    pick = dp[i - 1][target - arr[i]];
+                }
+                dp[i][target] = pick || notPick;
+            }
+        }
+
+        return dp[n - 1][sum];
+    }
+}
+
 
 //memoization
 class Solution {
