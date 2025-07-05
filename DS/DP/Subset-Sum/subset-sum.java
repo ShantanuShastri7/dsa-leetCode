13f071acadc4e057bba4b98361775aa75c7fe084
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-05 09:58:29 +0530

    Update subset-sum.java

--- DS/DP/Subset-Sum/subset-sum.java
+++ DS/DP/Subset-Sum/subset-sum.java
@@ -1,3 +1,37 @@
+
+//memoization
+class Solution {
+
+    public Boolean isSubsetSum(int arr[], int sum) {
+        int dp[][] = new int[arr.length][sum+1];
+        for(int i=0; i<arr.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        helper(arr, sum, arr.length-1, dp);
+        
+        return dp[arr.length-1][sum]==1;
+    }
+    
+    private int helper(int arr[], int target, int index, int dp[][]){
+        if(target==0) return 1;
+        
+        if(index==0) {
+            return target==arr[index]?1:0;
+        }
+        
+        if(dp[index][target]!=-1) return dp[index][target];
+        
+        int notPick = helper(arr, target, index-1, dp);
+        int pick = 0;
+        if(target>=arr[index]){
+            pick = helper(arr, target-arr[index], index-1, dp);
+        }
+        
+        return dp[index][target] = (pick==1 || notPick==1)?1:0;
+    }
+}
+
+//recursive dp -> TLE
 class Solution {
 
     public Boolean isSubsetSum(int arr[], int sum) {
