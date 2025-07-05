9bbaa314dc978ac347deef60e03ba3aac2897a1d
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-05 14:59:24 +0530

    Update partition-with-given-difference.java

--- DS/DP/PartitionWithGivenDifference(DP-19/partition-with-given-difference.java
+++ DS/DP/PartitionWithGivenDifference(DP-19/partition-with-given-difference.java
@@ -1,4 +1,39 @@
+// tabulation
+class Solution {
+    int countPartitions(int[] arr, int d) {
+        int total = Arrays.stream(arr).sum();
+        if (total < d || (total - d) % 2 != 0) return 0;
 
+        int target = (total - d) / 2;
+        int[][] dp = new int[arr.length][target + 1];
+
+        if (arr[0] == 0) {
+            dp[0][0] = 2;  
+        } else {
+            dp[0][0] = 1; 
+        }
+
+        if (arr[0] != 0 && arr[0] <= target) {
+            dp[0][arr[0]] = 1; 
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int sum = 0; sum <= target; sum++) {
+                int notTake = dp[i - 1][sum];
+                int take = 0;
+                if (arr[i] <= sum) {
+                    take = dp[i - 1][sum - arr[i]];
+                }
+                dp[i][sum] = take + notTake;
+            }
+        }
+
+        return dp[arr.length - 1][target];
+    }
+}
+
+
+//recursive -> TLE
 class Solution {
     int countPartitions(int[] arr, int d) {
         int total = Arrays.stream(arr).sum();
