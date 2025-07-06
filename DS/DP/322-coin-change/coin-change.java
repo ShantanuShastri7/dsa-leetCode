a2cfd80f1a67ad2fa3f08d2ec143fc8fec824506
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-06 23:23:30 +0530

    Time: 17 ms (47.52%) | Memory: 45.5 MB (17.93%) - LeetSync

--- DS/DP/322-coin-change/coin-change.java
+++ DS/DP/322-coin-change/coin-change.java
@@ -2,11 +2,21 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
         int dp[][] = new int[coins.length][amount+1];
-        for(int i=0; i<coins.length; i++){
-            Arrays.fill(dp[i],-1);
+        for(int i=0; i<=amount; i++){
+            if(i%coins[0]==0) dp[0][i]=i/coins[0];
+            else dp[0][i]=1000000;
         }
-        int answer = helper(coins, amount, coins.length-1, dp);
-        return answer>=1000000?-1:answer;
+        for(int i=1; i<coins.length; i++){
+            for(int j=0; j<=amount; j++){
+                int notPick = dp[i-1][j];
+                int pick=1000000;
+                if(coins[i]<=j){
+                    pick=1+dp[i][j-coins[i]];
+                }
+                dp[i][j]=Math.min(pick,notPick);
+            }
+        }
+        return dp[coins.length-1][amount]==1000000?-1:dp[coins.length-1][amount];
     }
 
     private int helper(int[] coins, int amount, int index,int dp[][]){
