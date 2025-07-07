176eec23b249a301f73dd17f31967ab206e96dc1
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-07 21:20:33 +0530

    Time: 9 ms (61.63%) | Memory: 49.5 MB (26.01%) - LeetSync

---
+++ DS/DP/518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        if(amount==0) return 1;
+        if(coins.length==1 && amount%coins[0]==0) return 1;
+
+        int dp[][] = new int[coins.length][amount+1];
+        for(int i=0; i<coins.length; i++) Arrays.fill(dp[i], -1);
+        helper(amount, coins, coins.length-1, dp);
+
+        return dp[coins.length-1][amount]==-1?0:dp[coins.length-1][amount];
+    }
+
+    private int helper(int amount, int [] coins, int index, int dp[][]){
+        if(amount==0) return 1;
+        if(index==0){
+            if(amount%coins[0]==0) return 1;
+            return 0;
+        }
+        
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        
+        int notPick = helper(amount, coins, index-1, dp);
+        int pick=0;
+        if(amount>=coins[index]){
+            pick = helper(amount-coins[index], coins, index, dp);
+        }
+
+        return dp[index][amount] = pick+notPick;
+    }
+}
