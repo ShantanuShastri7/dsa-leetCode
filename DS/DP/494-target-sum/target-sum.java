f65306c4b00a8d835f7553a970353f9c65597199
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-07 19:38:12 +0530

    Time: 197 ms (36.00%) | Memory: 46 MB (5.98%) - LeetSync

--- DS/DP/494-target-sum/target-sum.java
+++ DS/DP/494-target-sum/target-sum.java
@@ -1,18 +1,23 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, nums.length-1);
+        HashMap<String, Integer> dp = new HashMap<>();
+        return helper(nums, target, nums.length - 1, dp);
     }
 
-    private int helper(int[] nums, int target, int index){
-        if(index==0){
-            if(target==0 && nums[0]==0) return 2;
-            if(Math.abs(target)==Math.abs(nums[0])) return 1;
-            else return 0;
+    private int helper(int[] nums, int target, int index, HashMap<String, Integer> dp) {
+        if (index == 0) {
+            if (target == 0 && nums[0] == 0)
+                return 2;
+            if (Math.abs(target) == Math.abs(nums[0]))
+                return 1;
+            else
+                return 0;
         }
-
-        int addition = helper(nums, target-nums[index], index-1);
-        int substraction = helper(nums, target+nums[index], index-1);
-
-        return addition+substraction;
+        if (dp.containsKey(Integer.toString(index) + ":" + Integer.toString(target)))
+            return dp.get(Integer.toString(index) + ":" + Integer.toString(target));
+        int addition = helper(nums, target - nums[index], index - 1, dp);
+        int substraction = helper(nums, target + nums[index], index - 1, dp);
+        dp.put(Integer.toString(index) + ":" + Integer.toString(target), addition + substraction);
+        return dp.get(Integer.toString(index) + ":" + Integer.toString(target));
     }
 }
