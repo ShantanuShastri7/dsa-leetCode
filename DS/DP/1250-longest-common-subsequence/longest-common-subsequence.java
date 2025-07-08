10fff8e36f341cbe1f645efb1fa1fcb7e61afa1c
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-08 21:56:12 +0530

    Time: 23 ms (36.95%) | Memory: 51.2 MB (30.11%) - LeetSync

--- DS/DP/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DS/DP/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,20 +1,39 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int i=0; i<text1.length(); i++){
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+
+        for (int i = 0; i <= text1.length(); i++) {
+            dp[i][0] = 0;
         }
-        return helper(text1,text2, text1.length()-1, text2.length()-1, dp);
+        for (int i = 0; i <= text2.length(); i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1],
+                            dp[i - 1][j]);
+                }
+            }
+        }
+        
+        return dp[text1.length()][text2.length()];
     }
 
-    private int helper(String text1, String text2, int index1, int index2, int[][] dp){
-        if(index1<0 || index2<0) return 0;
+    private int helper(String text1, String text2, int index1, int index2, int[][] dp) {
+        if (index1 < 0 || index2 < 0)
+            return 0;
 
-        if(dp[index1][index2]!=-1) return dp[index1][index2];
-        if(text1.charAt(index1)==text2.charAt(index2)){
-            return dp[index1][index2]=1+ helper(text1, text2, index1-1, index2-1, dp);
-        } else{
-            return dp[index1][index2]=Math.max(helper(text1, text2, index1, index2-1,dp),helper(text1, text2, index1-1, index2,dp));
+        if (dp[index1][index2] != -1)
+            return dp[index1][index2];
+
+        if (text1.charAt(index1) == text2.charAt(index2)) {
+            return dp[index1][index2] = 1 + helper(text1, text2, index1 - 1, index2 - 1, dp);
+        } else {
+            return dp[index1][index2] = Math.max(helper(text1, text2, index1, index2 - 1, dp),
+                    helper(text1, text2, index1 - 1, index2, dp));
         }
     }
 }
