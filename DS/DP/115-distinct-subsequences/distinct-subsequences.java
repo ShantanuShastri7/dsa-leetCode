90e0b454405588afe1a0cee25b711a3eabca7d74
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-10 23:34:06 +0530

    Time: 22 ms (21.81%) | Memory: 51.4 MB (30.60%) - LeetSync

--- DS/DP/115-distinct-subsequences/distinct-subsequences.java
+++ DS/DP/115-distinct-subsequences/distinct-subsequences.java
@@ -1,11 +1,25 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int dp[][] = new int[s.length()][t.length()];
-        for(int i=0; i<s.length(); i++){
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
         }
-        helper(s, t, s.length()-1, t.length()-1, dp);
-        return dp[s.length()-1][t.length()-1];
+        for (int j = 1; j <= t.length(); j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[s.length()][t.length()];
     }
 
     private int helper(String s, String t, int i, int j, int dp[][]){
