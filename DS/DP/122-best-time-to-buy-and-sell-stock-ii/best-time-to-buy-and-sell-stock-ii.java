f9dd75e98e78fa45ca8a2aabd4606515324bba3b
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-13 12:46:30 +0530

    Time: 1 ms (78.42%) | Memory: 46.1 MB (36.50%) - LeetSync

--- DS/DP/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ DS/DP/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,26 +1,39 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
-        for (int i = 0; i < prices.length; i++) {
-            Arrays.fill(dp[i], -1);
+        int dp[][] = new int[prices.length+1][2];
+        dp[prices.length - 1][1] = 0;
+        dp[prices.length - 1][0] = 0;
+
+        for (int i = prices.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= 1; j++) {
+                if (j == 0) {
+                    int profit = Math.max((dp[i+1][1] - prices[i]), dp[i+1][0]);
+                    dp[i][j] = profit;
+                } else {
+                    int profit = Math.max((prices[i] + dp[i+1][0]), dp[i+1][1]);
+                    dp[i][j] = profit;
+                }
+            }
         }
-        return helper(prices, 0, 0, dp);
+
+        return dp[0][0];
     }
 
     private int helper(int[] prices, int index, int buy, int dp[][]) {
         if (index == prices.length)
             return 0;
 
-        if(dp[index][buy]!=-1) return dp[index][buy];
+        if (dp[index][buy] != -1)
+            return dp[index][buy];
 
         if (buy == 0) {
             int profit = Math.max((helper(prices, index + 1, 1, dp) - prices[index]),
                     (helper(prices, index + 1, 0, dp)));
-            return dp[index][buy] =profit;
+            return dp[index][buy] = profit;
         } else {
-            int profit = Math.max((prices[index] + helper(prices, index + 1, 0, dp)), 
+            int profit = Math.max((prices[index] + helper(prices, index + 1, 0, dp)),
                     helper(prices, index + 1, 1, dp));
-            return dp [index][buy] = profit;
+            return dp[index][buy] = profit;
         }
     }
 
