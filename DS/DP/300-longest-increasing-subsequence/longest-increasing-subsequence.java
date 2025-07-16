ab8273a3d221adfdaa9c9b1e840a4fa9796b029d
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-16 08:57:45 +0530

    Time: 74 ms (25.09%) | Memory: 79 MB (12.98%) - LeetSync

--- DS/DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DS/DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,15 +1,28 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return helper(nums, 0, -1, dp);
+        int[][] dp = new int[n+1][n+1];
+
+        for (int i = n-1; i>=0; i--) {
+            for (int j = i-1; j>=-1; j--) {
+                int notTake = dp[i+1][j+1];
+                int take = 0;
+                if (j == -1 || nums[i] > nums[j]) {
+                    take = 1 + dp[i+1][i+1];
+                }
+
+                dp[i][j + 1] = Math.max(take, notTake);
+            }
+        }
+        return dp[0][0];
     }
 
     private int helper(int[] nums, int index, int prevIndex, int[][] dp) {
-        if (index == nums.length) return 0;
+        if (index == nums.length)
+            return 0;
 
-        if (dp[index][prevIndex + 1] != -1) return dp[index][prevIndex + 1];
+        if (dp[index][prevIndex + 1] != -1)
+            return dp[index][prevIndex + 1];
 
         int notTake = helper(nums, index + 1, prevIndex, dp);
         int take = 0;
