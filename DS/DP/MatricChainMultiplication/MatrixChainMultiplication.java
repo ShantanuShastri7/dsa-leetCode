05720bb14b470d5c2399d81ab0268839d21689e0
Shantanu Shastri <shantanushastri70@gmail.com>
2025-07-21 00:15:50 +0530

    Update MatrixChainMultiplication.java

--- DS/DP/MatricChainMultiplication/MatrixChainMultiplication.java
+++ DS/DP/MatricChainMultiplication/MatrixChainMultiplication.java
@@ -1,4 +1,27 @@
 class Solution {
+
+    //Tabulation
+    public int matrixMultiplication(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        for(int i=n-1; i>=1; i--){
+            for(int j=i+1; j<=n-1; j++){
+                int min=Integer.MAX_VALUE;
+                for(int k=i; k<j; k++){
+                    int cost = arr[i-1]*arr[k]*arr[j]
+                                + dp[i][k]
+                                + dp[k+1][j];
+                    
+                    min = Math.min(min, cost);
+                }
+                dp[i][j]=min;
+            }
+        }
+        
+        return dp[1][n-1];
+    }
+
+    //Memoization
     public int matrixMultiplication(int arr[]) {
         int n = arr.length;
         int dp[][] = new int[n][n];
