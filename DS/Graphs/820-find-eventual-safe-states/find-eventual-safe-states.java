2a76e1255de927c4d7da1a27108914f13a14d7cf
Shantanu Shastri <shantanushastri70@gmail.com>
2025-08-14 08:58:25 +0530

    Time: 27 ms (21.45%) | Memory: 55.5 MB (56.68%) - LeetSync

--- DS/Graphs/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ DS/Graphs/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,29 +1,64 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         ArrayList<Integer> result = new ArrayList<>();
-        int[] visited = new int[graph.length];
+        ArrayList<ArrayList<Integer>> adjRev = new ArrayList<>();
+        int[] inorder = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            adjRev.add(new ArrayList<>());
+        }   
 
         for(int i=0; i<graph.length; i++){
-            if(dfs(graph, i, visited)){
-                result.add(i);
+            for(int j : graph[i]){
+                adjRev.get(j).add(i);
+                inorder[i]++;
             }
         }
+        Queue<Integer> q = new ArrayDeque<>();
+
+        for(int i = 0; i<adjRev.size(); i++){
+            if(inorder[i]==0){
+                q.offer(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            result.add(node);
+            for(int i : adjRev.get(node)){
+                --inorder[i];
+                if(inorder[i]==0) q.offer(i);
+            }
+        }
+        Collections.sort(result);
         return result;
     }
 
-    private boolean dfs(int[][] graph, int targetNode, int[] visited){
-        if(visited[targetNode]!=0){
-            return visited[targetNode]==2;
-        }
-        visited[targetNode]=1;
-        
-        for(int adj : graph[targetNode]){
-            if (!dfs(graph, adj, visited)) {
-                return false;
-            }
-        }
+    // public List<Integer> eventualSafeNodes(int[][] graph) {
+    //     ArrayList<Integer> result = new ArrayList<>();
+    //     int[] visited = new int[graph.length];
 
-        visited[targetNode]=2;
-        return true;
-    }
+    //     for(int i=0; i<graph.length; i++){
+    //         if(dfs(graph, i, visited)){
+    //             result.add(i);
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    // private boolean dfs(int[][] graph, int targetNode, int[] visited){
+    //     if(visited[targetNode]!=0){
+    //         return visited[targetNode]==2;
+    //     }
+    //     visited[targetNode]=1;
+
+    //     for(int adj : graph[targetNode]){
+    //         if (!dfs(graph, adj, visited)) {
+    //             return false;
+    //         }
+    //     }
+
+    //     visited[targetNode]=2;
+    //     return true;
+    // }
 }
