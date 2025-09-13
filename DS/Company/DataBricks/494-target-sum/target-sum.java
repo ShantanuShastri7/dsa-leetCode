280dc0dde74b0ea80c34d0ead59df8a0e58eccd5
Shantanu Shastri <shantanushastri70@gmail.com>
2025-09-13 08:43:13 -0400

    Time: 184 ms (32.53%) | Memory: 45.9 MB (5.88%) - LeetSync

--- DS/Company/DataBricks/494-target-sum/target-sum.java
+++ DS/Company/DataBricks/494-target-sum/target-sum.java
@@ -3,15 +3,23 @@ class Solution {
         if(nums.length==0) return 0;
         if(Arrays.stream(nums).sum()<target) return 0;
 
-        return helper(nums, 0, target, 0);
+        Map<String, Integer> map = new HashMap<>();
+
+        return helper(nums, 0, target, 0, map);
     }
 
-    private int helper(int[] nums, int sum, int target, int index){
+    private int helper(int[] nums, int sum, int target, int index, Map<String, Integer> map){
         if(index==nums.length && sum==target) return 1;
         if(index==nums.length && sum!=target) return 0;
 
-        int add = helper(nums, sum+nums[index], target, index+1);
-        int subtract = helper(nums, sum-nums[index], target, index+1);
+        if(map.containsKey(Integer.toString(index)+":"+Integer.toString(sum))) {
+            return map.get(Integer.toString(index)+":"+Integer.toString(sum));
+        }
+        
+        int add = helper(nums, sum+nums[index], target, index+1, map);
+        int subtract = helper(nums, sum-nums[index], target, index+1, map);
+
+        map.put(Integer.toString(index)+":"+Integer.toString(sum), add+subtract);
 
         return add+subtract;
     }
