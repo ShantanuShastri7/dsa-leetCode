0a9b702124c4a9994ce1b31e7b0813c00e34cb2e
Shantanu Shastri <shantanushastri70@gmail.com>
2025-09-13 01:06:50 -0400

    Time: 6 ms (10.63%) | Memory: 47.4 MB (89.89%) - LeetSync

--- DS/Company/DataBricks/64-minimum-path-sum/minimum-path-sum.java
+++ DS/Company/DataBricks/64-minimum-path-sum/minimum-path-sum.java
@@ -1,14 +1,39 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        if(grid.length==0) return 0;
-        if(grid.length==1 && grid[0].length==1) return grid[0][0];
+        int[][] dp = new int[grid.length][grid[0].length];
 
-        int[][] memo = new int[grid.length][grid[0].length];
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(i==0 && j==0) dp[i][j]=grid[0][0];
+                else {
 
-        for(int i=0; i<grid.length; i++) Arrays.fill(memo[i], -1);
+                    int up = grid[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += 999999;
 
-        return helper(grid, grid.length-1, grid[0].length-1, memo);
+                    int left = grid[i][j];
+                    if(j>0) left += dp[i][j-1];
+                    else left+=999999;
+
+                    dp[i][j] = Math.min(up, left);
+                }
+            }
+        }
+
+        return dp[grid.length-1][grid[0].length-1];
     }
+
+    //Memoization
+    // public int minPathSum(int[][] grid) {
+    //     if(grid.length==0) return 0;
+    //     if(grid.length==1 && grid[0].length==1) return grid[0][0];
+
+    //     int[][] memo = new int[grid.length][grid[0].length];
+
+    //     for(int i=0; i<grid.length; i++) Arrays.fill(memo[i], -1);
+
+    //     return helper(grid, grid.length-1, grid[0].length-1, memo);
+    // }
 
     private int helper(int[][] grid, int i, int j, int[][] memo){
         if(i<0 || j<0) return 99999;
