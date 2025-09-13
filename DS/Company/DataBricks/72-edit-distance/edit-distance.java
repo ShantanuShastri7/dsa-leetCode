b9351930028adee83d10f1a01b965a0763fd0427
Shantanu Shastri <shantanushastri70@gmail.com>
2025-09-13 16:44:24 -0400

    Time: 3 ms (94.95%) | Memory: 45.3 MB (18.58%) - LeetSync

---
+++ DS/Company/DataBricks/72-edit-distance/edit-distance.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        if(word1==word2) return 0;
+
+        int[][] dp = new int[word1.length()][word2.length()];
+
+        for(int i=0; i<word1.length(); i++) Arrays.fill(dp[i], -1);
+
+        return helper(word1, word2, word1.length()-1, word2.length()-1, dp);
+    }
+
+    private int helper(String word1, String word2, int i, int j, int[][] dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j] = helper(word1, word2, i-1, j-1, dp);
+        } else{
+            int delete = 1 + helper(word1, word2, i-1, j, dp);
+            int insert = 1 + helper(word1, word2, i, j-1, dp);
+            int replace = 1 + helper(word1, word2, i-1, j-1, dp);
+
+            return dp[i][j] = Math.min(delete, Math.min(insert, replace));
+        }
+    }
+}
