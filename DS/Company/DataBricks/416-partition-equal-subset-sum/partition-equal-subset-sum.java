6f87ed5e08af01e713c0012e76b117dff28c32ed
Shantanu Shastri <shantanushastri70@gmail.com>
2025-09-13 00:12:26 -0400

    Time: 48 ms (49.87%) | Memory: 54.9 MB (40.97%) - LeetSync

--- DS/Company/DataBricks/416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ DS/Company/DataBricks/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -3,14 +3,41 @@ class Solution {
         int totalSum = Arrays.stream(nums).sum();
         if(totalSum%2!=0) return false;
 
+        boolean[][] dp = new boolean[nums.length][totalSum+1];
         totalSum=totalSum/2;
 
-        int[][] memo = new int[nums.length+1][totalSum+1];
-        for(int i=0; i<=nums.length; i++) Arrays.fill(memo[i], -1);
+        for(int i=0; i<nums.length; i++){
+            dp[i][0]=true;
+        }
+        dp[0][nums[0]]=true;
 
-        return helper(nums, totalSum, 0, memo)==1;
+        for(int i=1; i<nums.length; i++){
+            for(int j=1; j<=totalSum; j++){
+                boolean notPick = dp[i-1][j];
+                boolean pick=false;
+                if(nums[i]<=j){
+                    pick = dp[i-1][j-nums[i]];
+                }
+
+                dp[i][j] = pick||notPick;
+            }
+        }
+
+        return dp[nums.length-1][totalSum];
 
     }
+    // public boolean canPartition(int[] nums) {
+    //     int totalSum = Arrays.stream(nums).sum();
+    //     if(totalSum%2!=0) return false;
+
+    //     totalSum=totalSum/2;
+
+    //     int[][] memo = new int[nums.length+1][totalSum+1];
+    //     for(int i=0; i<=nums.length; i++) Arrays.fill(memo[i], -1);
+
+    //     return helper(nums, totalSum, 0, memo)==1;
+
+    // }
 
     private int helper(int[] nums, int target, int index, int[][] memo){
         if(index==nums.length) return 0; 
