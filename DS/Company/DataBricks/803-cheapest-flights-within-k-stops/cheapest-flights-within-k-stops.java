94b5c53d411f65e56eea19e4e34c08fa17772cac
Shantanu Shastri <shantanushastri70@gmail.com>
2025-09-17 23:25:12 -0400

    Time: 189 ms (5.01%) | Memory: 46.3 MB (5.12%) - LeetSync

---
+++ DS/Company/DataBricks/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> adjacency = new ArrayList<>();
+        for(int i=0; i<n; i++) adjacency.add(new ArrayList<>());
+
+        for(int i=0; i<flights.length; i++){
+            adjacency.get(flights[i][0]).add(new int[]{flights[i][1], flights[i][2]});
+        }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, 999999);
+
+        PriorityQueue<Pair<Integer, Pair<Integer,Integer>>> pq=new PriorityQueue<>(Comparator.comparingInt(a -> 
+                                                                        a.getKey()));
+        dist[src]=0;
+        pq.offer(new Pair<>(0, new Pair<>(src, 0)));
+
+        while(!pq.isEmpty()){
+            Pair<Integer, Pair<Integer, Integer>> node = pq.poll();
+            int hops = node.getKey();
+            int source = node.getValue().getKey();
+            int dis = node.getValue().getValue();
+
+            if(hops>k) break;
+
+            for(int[] adj : adjacency.get(source)){
+                int next = adj[0];
+                int nextDis = adj[1];
+                System.out.print(" source: "+source+" next: "+next);
+                System.out.print(" dis: "+dis+" nextDis: "+nextDis+" distatNext: "+dist[next]);
+
+                if(dis+nextDis<dist[next] && hops<k+1){
+                    dist[next] = dis+nextDis;
+                    pq.offer(new Pair<>(hops+1, new Pair<>(next, dis+nextDis)));
+                }
+            }
+        }
+
+        return dist[dst]==999999?-1:dist[dst];
+    }
+}
