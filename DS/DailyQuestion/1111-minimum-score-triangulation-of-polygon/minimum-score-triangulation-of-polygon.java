8c4a469e4d316836c6495c8b1d6225bc6330e9af
Shantanu Shastri <shantanushastri70@gmail.com>
2025-09-30 20:22:05 -0400

    Time: 5 ms (56.05%) | Memory: 41.3 MB (47.93%) - LeetSync

--- DS/DailyQuestion/1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
+++ DS/DailyQuestion/1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -1,11 +1,33 @@
 class Solution {
     public int minScoreTriangulation(int[] values) {
-        int[][] dp = new int[values.length][values.length];
-        for(int i=0; i<values.length; i++){
-            Arrays.fill(dp[i], -1);
+        int n = values.length;
+        int[][] dp = new int[n][n];
+
+        // dp[i][j] = minimum triangulation score for polygon between i and j
+        for (int len = 2; len < n; len++) {          // length of the interval
+            for (int i = 0; i + len < n; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                // Try all possible middle points
+                for (int k = i + 1; k < j; k++) {
+                    int cost = dp[i][k] + dp[k][j] + values[i] * values[k] * values[j];
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
         }
-        return triangulate(values, 0, values.length - 1, dp);
+
+        return dp[0][n - 1];
     }
+
+    // Memiozation
+    // public int minScoreTriangulation(int[] values) {
+    //     int[][] dp = new int[values.length][values.length];
+    //     for(int i=0; i<values.length; i++){
+    //         Arrays.fill(dp[i], -1);
+    //     }
+    //     return triangulate(values, 0, values.length - 1, dp);
+    // }
 
     private int triangulate(int[] values, int start, int end, int[][] dp) {
         if (end - start < 2) return 0;
