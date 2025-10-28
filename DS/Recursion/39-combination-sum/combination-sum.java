dc31d9be7dc4612125f698567a53437ffe0ff64b
Shantanu Shastri <shantanushastri70@gmail.com>
2025-10-28 00:10:09 -0400

    Time: 2 ms (84.88%) | Memory: 44.9 MB (20.26%) - LeetSync

--- DS/Recursion/39-combination-sum/combination-sum.java
+++ DS/Recursion/39-combination-sum/combination-sum.java
@@ -1,37 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Integer[] arr = Arrays.stream(candidates).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr, Collections.reverseOrder());
-
-        Set<ArrayList<Integer>> res = new HashSet<>();
-        ArrayList<Integer> cur = new ArrayList<>();
-        helper(arr, target, 0, 0, res, cur);
-
-        List<List<Integer>> finalRes = new ArrayList<>();
-        Iterator<ArrayList<Integer>> it = res.iterator();
-        while (it.hasNext()) {
-            finalRes.add(it.next());
-        }
-        return finalRes;
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), res);
+        return res;
     }
 
-    private void helper(Integer[] candidates, int target, int index, int sum, Set<ArrayList<Integer>> result, ArrayList<Integer> curr){
-        if(index>=candidates.length){
-            if(sum==target){
-                result.add(new ArrayList<>(curr));
-                return;
-            } else  return;
+    private void backtrack(int[] candidates, int target, int index, List<Integer> curr, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
         }
 
-        if(target-sum>=candidates[index]){
-            helper(candidates, target, index+1, sum, result, curr);
-            curr.add(candidates[index]);
-            sum+=candidates[index];
-            helper(candidates, target, index, sum, result, curr);
-            sum-=candidates[index];
-            curr.remove(curr.size()-1);
-        } else {
-            helper(candidates, target, index+1, sum, result, curr);
+        for (int i = index; i < candidates.length; i++) {
+            if (candidates[i] > target) break;
+            curr.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], i, curr, res);
+            curr.remove(curr.size() - 1);
         }
     }
 }
