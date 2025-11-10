b8c114c224968418b540f29a2c953cc41c3a290e
Shantanu Shastri <shantanushastri70@gmail.com>
2025-11-10 13:27:52 -0500

    Implement binary search for allocating pages

--- DS/BinarySearch/AllocateMininumPages/code.java
+++ DS/BinarySearch/AllocateMininumPages/code.java
@@ -35,3 +35,50 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+
+#2
+    class Solution {
+    public int findPages(int[] arr, int k) {
+        int low = Arrays.stream(arr).max().getAsInt();;
+        int high = Arrays.stream(arr).sum();
+        int pgs=-1;
+        
+        if(k>arr.length) return -1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            
+            boolean result = isThisPossible(arr, k, mid);
+            if(result){
+               pgs=mid;
+               high=mid-1;
+            } else{
+                low = mid+1;
+            }
+        }
+        
+        return pgs;
+        
+    }
+    
+    
+    private boolean isThisPossible(int[] arr, int k, int pages){
+        int i=0;
+        
+        while(i<arr.length && k>0){
+            int sum=0;
+            if(arr[i]>pages) return false;
+            while(i<arr.length && sum+arr[i]<=pages){
+                sum+=arr[i];
+                i++;
+            }
+            k--;
+        }
+        if(i==arr.length) return true;
+        else return false;
+    }
+}
