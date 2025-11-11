4b1a4f5aa00d72d4d67885f1e81546e35af49533
Shantanu Shastri <shantanushastri70@gmail.com>
2025-11-11 16:02:14 -0500

    Add aggressiveCows method for cow placement problem
    Implement binary search to find the maximum distance for placing cows in stalls.

--- DS/BinarySearch/AggressiveCows/code.java
+++ DS/BinarySearch/AggressiveCows/code.java
@@ -32,3 +32,51 @@ class Solution {
         return cows<=0;
     }
 }
+
+
+
+
+
+#2
+    class Solution {
+    public int aggressiveCows(int[] stalls, int k) {
+        Arrays.sort(stalls);
+        int lowest = Integer.MAX_VALUE;
+        int high = (stalls[stalls.length-1] - stalls[0]);
+        
+        for(int i=1; i<stalls.length; i++){
+            lowest = Math.min(lowest, (stalls[i]-stalls[i-1]));
+        }
+        int low = lowest;
+        int result = 0;
+        
+        while(low<=high){
+            int middle = (low+high)/2;
+            
+            if(isPossible(stalls, k, middle)){
+                result = middle;
+                low = middle+1;
+            } else high = middle-1;
+        }
+        
+        return result;
+    }
+    
+    private boolean isPossible(int[] stalls, int k, int dist){
+        int i=0;
+        int placed=1;
+        while(i<stalls.length-1){
+            int initialStall=stalls[i];
+            while(i<stalls.length-1 && stalls[i+1]-initialStall<dist){
+                i++;
+            }
+            if(i<stalls.length-1) {
+                placed++;
+                i++;
+            }
+        }
+        
+        if(placed>=k) return true;
+        else return false;
+    }
+}
