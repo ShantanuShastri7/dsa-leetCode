a0858957a94464281a82413e723cef3156f81ff4
Shantanu Shastri <shantanushastri70@gmail.com>
2026-01-27 14:33:19 -0500

    Time: 4 ms (2.40%) | Memory: 79.3 MB (11.25%) - LeetSync

--- DS/Array/53-maximum-subarray/maximum-subarray.java
+++ DS/Array/53-maximum-subarray/maximum-subarray.java
@@ -1,21 +1,66 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // Initialize maxSoFar and maxEndingHere to the first element
-        int maxSoFar = nums[0];
-        int maxEndingHere = nums[0];
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, Integer.MIN_VALUE);
 
-        // Iterate through the array starting from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // Either start a new subarray at nums[i] or extend the previous subarray
-            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
-
-            // Update the global maximum
-            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            //Trying to find the max sub array ending at i
+            max = Math.max(max, helper(nums, i, memo));
         }
+        return max;
+    }
 
-        return maxSoFar;
+    private int helper(int[] nums, int i, int[] memo) {
+        //if i==0 there can be only that number
+        if (i == 0) return nums[0];
+        if (memo[i] != Integer.MIN_VALUE) return memo[i];
+
+        //essentially checking if the max is the number or the number plus max from previous
+        //options
+        memo[i] = Math.max(nums[i], nums[i] + helper(nums, i - 1, memo));
+        return memo[i];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         // Initialize maxSoFar and maxEndingHere to the first element
+//         int maxSoFar = nums[0];
+//         int maxEndingHere = nums[0];
+
+//         // Iterate through the array starting from the second element
+//         for (int i = 1; i < nums.length; i++) {
+//             // Either start a new subarray at nums[i] or extend the previous subarray
+//             maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+
+//             // Update the global maximum
+//             maxSoFar = Math.max(maxSoFar, maxEndingHere);
+//         }
+
+//         return maxSoFar;
+//     }
+// }
 
 // class Solution {
 //     public int maxSubArray(int[] nums) {
