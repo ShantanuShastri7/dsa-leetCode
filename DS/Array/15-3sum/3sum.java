939e2436e1a35d1a7ea9e928dfab6a9545f6f771
Shantanu Shastri <shantanushastri70@gmail.com>
2026-01-28 08:58:55 -0500

    Time: 34 ms (58.81%) | Memory: 59 MB (81.59%) - LeetSync

--- DS/Array/15-3sum/3sum.java
+++ DS/Array/15-3sum/3sum.java
@@ -1,22 +1,65 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-        Set<List<Integer>> result = new HashSet<>();
 
-        for(int i=0; i<n; i++){
-            int rem = -nums[i];
-            Map<Integer, Integer> list = new HashMap<>();
+        for (int i = 0; i < n - 2; i++) {
 
-            for(int j=i+1; j<n; j++){
-                if(list.containsKey(rem-nums[j])){
-                    list.put(nums[j], 0);
-                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], rem-nums[j])));
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
-                list.put(nums[j], 0);
             }
         }
 
-        return new ArrayList<>(result);
+        return result;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         Set<List<Integer>> result = new HashSet<>();
+
+//         for(int i=0; i<n; i++){
+//             int rem = -nums[i];
+//             Map<Integer, Integer> list = new HashMap<>();
+
+//             for(int j=i+1; j<n; j++){
+//                 if(list.containsKey(rem-nums[j])){
+//                     list.put(nums[j], 0);
+//                     result.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], rem-nums[j])));
+//                 }
+//                 list.put(nums[j], 0);
+//             }
+//         }
+
+//         return new ArrayList<>(result);
+//     }
+// }
