1266db52f46dc96578ad982eb8d48bbb1445bd30
Shantanu Shastri <shantanushastri70@gmail.com>
2026-05-27 08:13:35 -0400

    Time: 1 ms (100.00%) | Memory: 43.3 MB (97.88%) - LeetSync

--- DS/DailyQuestion/3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ DS/DailyQuestion/3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,35 +1,62 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int[] occur = new int[26];
-        int special=0;
+        boolean[] hasLower = new boolean[26];
+        boolean[] hasUpper = new boolean[26];
 
-        for(int i=0; i<word.length(); i++){
-            int cap=0;
-            int charIndex = (int) word.charAt(i);
-            if(charIndex>=97){
-                charIndex-=97;
-            } else{
-                cap=1;
-                charIndex-=65;
+        // 1. Record all occurrences
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                hasLower[c - 'a'] = true;
+            } else if (c >= 'A' && c <= 'Z') {
+                hasUpper[c - 'A'] = true;
             }
+        }
 
-            if(occur[charIndex]==1 && cap==1){
+        // 2. Count characters that appear in both
+        int special = 0;
+        for (int i = 0; i < 26; i++) {
+            if (hasLower[i] && hasUpper[i]) {
                 special++;
-                occur[charIndex]=3;
-            } else if(occur[charIndex]==2 && cap==0){
-                special++;
-                occur[charIndex]=3;
-            }
-
-            if(occur[charIndex]!=3){
-                if(cap==1){
-                    occur[charIndex]=2;
-                }else{
-                    occur[charIndex]=1;
-                }
             }
         }
 
         return special;
     }
 }
+
+// class Solution {
+//     public int numberOfSpecialChars(String word) {
+//         int[] occur = new int[26];
+//         int special=0;
+
+//         for(int i=0; i<word.length(); i++){
+//             int cap=0;
+//             int charIndex = (int) word.charAt(i);
+//             if(charIndex>=97){
+//                 charIndex-=97;
+//             } else{
+//                 cap=1;
+//                 charIndex-=65;
+//             }
+
+//             if(occur[charIndex]==1 && cap==1){
+//                 special++;
+//                 occur[charIndex]=3;
+//             } else if(occur[charIndex]==2 && cap==0){
+//                 special++;
+//                 occur[charIndex]=3;
+//             }
+
+//             if(occur[charIndex]!=3){
+//                 if(cap==1){
+//                     occur[charIndex]=2;
+//                 }else{
+//                     occur[charIndex]=1;
+//                 }
+//             }
+//         }
+
+//         return special;
+//     }
+// }
