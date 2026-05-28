56a0e761706ae8bde3e69883909a8323f5cfcff4
Shantanu Shastri <shantanushastri70@gmail.com>
2026-05-28 08:27:45 -0400

    Time: 2 ms (70.36%) | Memory: 47.4 MB (32.25%) - LeetSync

--- DS/Array/152-maximum-product-subarray/maximum-product-subarray.java
+++ DS/Array/152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,58 +1,25 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int prefix = 1;
-        int suffix = 1; 
-        int result=Integer.MIN_VALUE;
+        int runningMax = nums[0];
+        int runningMin = nums[0];
+        int result = nums[0];
 
-        for(int i=0; i<nums.length; i++){
-            prefix*=nums[i];
-            suffix*=nums[nums.length-i-1];
-            result=Math.max(result, Math.max(prefix, suffix));
+        for (int i = 1; i < nums.length; i++) {
+            int current = nums[i];
 
-            if(nums[i]==0) prefix=1;
-            if(nums[nums.length-i-1]==0) suffix=1;
+            // Calculate all three candidates
+            int cand1 = current;
+            int cand2 = runningMax * current;
+            int cand3 = runningMin * current;
+
+            // Update runningMax and runningMin simultaneously
+            runningMax = Math.max(cand1, Math.max(cand2, cand3));
+            runningMin = Math.min(cand1, Math.min(cand2, cand3));
+
+            // Update the global result
+            result = Math.max(result, runningMax);
         }
 
         return result;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public int maxProduct(int[] nums) {
-//         int max = Integer.MIN_VALUE;
-//         int prod = 1;
-
-//         for (int i = 0; i < nums.length; i++) {
-//             prod *= nums[i];
-//             max = Math.max(max, prod);
-//             if (nums[i] == 0)
-//                 prod = 1;
-//         }
-
-//         prod = 1;
-//         for (int i = nums.length - 1; i >= 0; i--) {
-//             prod *= nums[i];
-//             max = Math.max(max, prod);
-//             if (nums[i] == 0)
-//                 prod = 1;
-//         }
-
-//         return max;
-//     }
-// }
