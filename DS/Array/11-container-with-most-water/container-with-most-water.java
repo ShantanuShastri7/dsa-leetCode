eebbfe81784b44ace60cfec92ca7a71b41a60fc0
Shantanu Shastri <shantanushastri70@gmail.com>
2026-06-07 13:04:56 -0400

    Time: 3 ms (97.63%) | Memory: 77.2 MB (83.32%) - LeetSync

--- DS/Array/11-container-with-most-water/container-with-most-water.java
+++ DS/Array/11-container-with-most-water/container-with-most-water.java
@@ -1,54 +1,53 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left=0; 
+        int left=0;
         int right=height.length-1;
-        int result=0;
+        int maxArea = 0;
 
-        while(left<=right){
-            int leftH = height[left];
-            int rightH = height[right];
-            if(leftH<=rightH){
-                result = Math.max(result, (right-left)*leftH);
+        while(left<right){
+            if(height[left]<=height[right]){
+                maxArea = Math.max(maxArea, (right-left)*height[left]);
                 left++;
-            } else {
-                result = Math.max(result, (right-left)*rightH);
+            } else{
+                maxArea = Math.max(maxArea, (right-left)*height[right]);
                 right--;
             }
         }
 
-        return result;
+        return maxArea;
     }
 }
 
+//The below solution ignores the width and just accounts for the longest bars left and right, this can be used to solve the Trappping Rain Water problem
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int n = height.length;
+//         ArrayList<int[]> greatestLeft = new ArrayList<>(); 
+//         ArrayList<int[]> greatestRight = new ArrayList<>();
+//         greatestLeft.add(new int[]{0, height[0]});
+//         greatestRight.add(new int[]{n-1, height[n-1]});
 
+//         for(int i=1; i<n; i++){
+//             int index = greatestLeft.get(i-1)[1]>height[i]?greatestLeft.get(i-1)[0]:i;
+//             greatestLeft.add(new int[]{index, Math.max(greatestLeft.get(i-1)[1], height[i])});
+//         }
 
+//         for(int i=n-2; i>=0; i--){
+//             int index = greatestRight.get(n-i-2)[1]>height[i]?greatestRight.get(n-i-2)[0]:i;
+//             greatestRight.add(new int[]{index, Math.max(greatestRight.get(n-i-2)[1], height[i])});
+//         }
 
+//         int result =0;
 
-
-
-
-
-
-
-
-
-
-
-// public int maxArea(int[] height) {
-//         int left=0; 
-//         int right=height.length-1;
-
-//         int result = (right-left)*Math.min(height[left],height[right]);
-
-//         while(left<right){
-//             if(height[left]<height[right]){
-//                 left++;
-//                 result = Math.max(result, (right-left)*Math.min(height[left],height[right]));
-//             } else {
-//                 right--;
-//                 result = Math.max(result, (right-left)*Math.min(height[left],height[right]));
-//             }
+//         for(int i=0; i<n; i++){
+//             int indexLeft = greatestLeft.get(i)[0];
+//             int valueLeft = greatestLeft.get(i)[1];
+//             int indexRight = greatestRight.get(n-i-1)[0];
+//             int valueRight = greatestRight.get(n-i-1)[1];
+//             System.out.print("indexLeft: "+indexLeft + " indexRight: "+indexRight + " valueLeft: "+valueLeft + " valueRIght: " + valueRight + "\n");
+//             result = Math.max(result, (indexRight-indexLeft)*Math.min(valueLeft, valueRight));
 //         }
 
 //         return result;
 //     }
+// }
