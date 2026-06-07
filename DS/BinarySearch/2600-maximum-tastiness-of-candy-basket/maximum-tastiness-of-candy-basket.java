b255b9f035395f8a61eaf0da586da7dd096f40de
Shantanu Shastri <shantanushastri70@gmail.com>
2026-06-07 14:07:57 -0400

    Time: 57 ms (6.78%) | Memory: 66.1 MB (20.90%) - LeetSync

---
+++ DS/BinarySearch/2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        
+        int min = 0;
+        int max = price[price.length-1] - price[0];
+        int answer =0;
+
+        while(min<=max){
+            int mid = (min+max)/2;
+            System.out.print("mid: " + mid + "\n");
+            if(helper(price, k, mid)){
+                System.out.print("Can Pick \n");
+                answer=Math.max(answer, mid);
+                min=mid+1;
+            } else{
+                max=mid-1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean helper(int[] prices, int k, int gap){
+        int left=0;
+        int right=1;
+        int canPick=1;
+
+        while(right<prices.length){
+            if(prices[right]-prices[left]>=gap){
+                canPick++;
+                left=right;
+                right++;
+            }else{
+                right++;
+            }
+        }
+
+        return canPick>=k;
+    }
+}
