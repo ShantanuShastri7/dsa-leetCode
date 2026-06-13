ab9f62570cbbe092235af292cb81d9605e052ea9
Shantanu Shastri <shantanushastri70@gmail.com>
2026-06-13 18:08:17 -0400

    Time: 16 ms (34.87%) | Memory: 53.9 MB (59.80%) - LeetSync

--- DS/DP/518-coin-change-ii/coin-change-ii.java
+++ DS/DP/518-coin-change-ii/coin-change-ii.java
@@ -1,51 +1,35 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        if (amount == 0)
-            return 1;
-        if (coins.length == 1 && amount % coins[0] == 0)
-            return 1;
-
-        int dp[][] = new int[coins.length][amount + 1];
-        for(int i=0; i<=amount; i++){
-            if(i%coins[0]==0) dp[0][i]=1;
-            else dp[0][i]=0;
-        }
-        for(int i=0; i<coins.length; i++){
-            dp[i][0]=1;
+        int[][] mem = new int[amount + 1][coins.length];
+        for (int i = 0; i < amount + 1; i++) {
+            Arrays.fill(mem[i], -1);
         }
 
-        for(int i=1; i<coins.length; i++){
-            for(int j=0; j<=amount; j++){
-                int notPick = dp[i-1][j];
-                int pick = 0;
-                if (j >= coins[i]) {
-                    pick = dp[i][j-coins[i]];
-                }
-                dp[i][j]=pick+notPick;
-            }
-        }
-
-        return dp[coins.length - 1][amount];
+        return helper(amount, coins, coins.length - 1, mem);
     }
 
-    private int helper(int amount, int[] coins, int index, int dp[][]) {
+    private int helper(int amount, int[] coins, int index, int[][] mem) {
         if (amount == 0)
             return 1;
         if (index == 0) {
-            if (amount % coins[0] == 0)
+            if (amount == 0)
                 return 1;
-            return 0;
+            else if (amount % coins[index] == 0)
+                return 1;
+            else
+                return 0;
         }
 
-        if (dp[index][amount] != -1)
-            return dp[index][amount];
+        if (mem[amount][index] != -1)
+            return mem[amount][index];
 
-        int notPick = helper(amount, coins, index - 1, dp);
         int pick = 0;
         if (amount >= coins[index]) {
-            pick = helper(amount - coins[index], coins, index, dp);
+            pick = helper(amount - coins[index], coins, index, mem);
         }
 
-        return dp[index][amount] = pick + notPick;
+        int notPick = helper(amount, coins, index - 1, mem);
+
+        return mem[amount][index] = pick + notPick;
     }
 }
