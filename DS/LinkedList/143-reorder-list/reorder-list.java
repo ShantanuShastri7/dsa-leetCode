e7dad1c06da0d90ab02c7c046ae5912cb6f976e7
Shantanu Shastri <shantanushastri70@gmail.com>
2026-07-08 20:27:58 -0400

    Time: 5 ms (5.41%) | Memory: 52.5 MB (5.73%) - LeetSync

---
+++ DS/LinkedList/143-reorder-list/reorder-list.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        Queue<ListNode> q = new LinkedList<>();
+        ListNode result = head;
+
+        helper(result, q, 0);
+
+        while (!q.isEmpty()) {
+            ListNode nextToNext = result.next;
+            ListNode fromEnd = q.poll();
+
+            if (result == fromEnd) {
+                result.next = null;
+                break;
+            }
+
+            result.next = fromEnd;
+
+            //To break even length issues
+            if (nextToNext == fromEnd) {
+                fromEnd.next = null;
+                break;
+            }
+
+            fromEnd.next = nextToNext;
+            result = nextToNext;
+        }
+
+        if(result.next !=null && result.next.next!=null && result.next.next==result) result.next=null;
+    }
+
+    private int helper(ListNode head, Queue<ListNode> q, int count) {
+        if (head == null)
+            return count / 2;
+
+        int leftToAdd = helper(head.next, q, count + 1);
+
+        if (leftToAdd > 0)
+            q.offer(head);
+
+        return leftToAdd - 1;
+    }
+}
