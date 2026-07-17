ff30078fe8538c397903cb184ef14b7a4e1056fa
Shantanu Shastri <shantanushastri70@gmail.com>
2026-07-17 18:14:20 -0400

    Time: 2 ms (99.03%) | Memory: 46.8 MB (7.01%) - LeetSync

---
+++ DS/Trees/572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+        return comp(root, subRoot);
+    }
+
+    private boolean comp(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+
+        if (compare(root, subRoot)) {
+            return true;
+        }
+
+        return comp(root.left, subRoot) || comp(root.right, subRoot);
+    }
+
+    private boolean compare(TreeNode root, TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        if(root==null || subRoot==null || root.val!=subRoot.val) return false;
+
+        return compare(root.left, subRoot.left) && compare(root.right, subRoot.right);
+    }
+}
