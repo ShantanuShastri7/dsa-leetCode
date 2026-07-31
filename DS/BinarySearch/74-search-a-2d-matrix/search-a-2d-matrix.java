6fd877d55bee423c1a01641257251a1b1029d811
Shantanu Shastri <shantanushastri70@gmail.com>
2026-07-31 17:45:38 -0400

    Time: 0 ms (100.00%) | Memory: 43.8 MB (62.93%) - LeetSync

--- DS/BinarySearch/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ DS/BinarySearch/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,36 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix.length==1 && matrix[0].length==1) return matrix[0][0]==target;
-        int i=0; 
-        int j=matrix.length-1;
-        int rowNum=-1;
 
-        while(i<=j){
-            int mid = (i+j)/2;
+        int top=0;
+        int down=matrix.length-1;
+        int mid=0;
+        int res=0;
+        while(top<=down){
+            mid=top+(down-top)/2;
 
             if(matrix[mid][0]>target){
-                j=mid-1;
-            } else if(matrix[mid][0]<target){
-                rowNum=mid;
-                i=mid+1;
-            } else return true;
+                down=mid-1;
+            }else{
+                top=mid+1;
+                res=mid;
+            }
         }
-        if(rowNum==-1) return false;
 
-        int[] row = matrix[rowNum];
+        int left=0;
+        int right=matrix[0].length-1;
+        int rowMid=0;
+        int res2=0;
+        while(left<=right){
+            rowMid=left+(right-left)/2;
 
-        i=0; 
-        j=matrix[0].length-1;
-
-        while(i<=j){
-            int mid = (i+j)/2;
-
-            if(row[mid]<target){
-                i=mid+1;
-            } else if(row[mid]>target){
-                j=mid-1;
-            } else return true;
+            if(matrix[res][rowMid]>target){
+                right=rowMid-1;
+            }else{
+                left=rowMid+1;
+                res2=rowMid;
+            }
         }
+
+        if(matrix[res][res2]==target) return true;
 
         return false;
     }
