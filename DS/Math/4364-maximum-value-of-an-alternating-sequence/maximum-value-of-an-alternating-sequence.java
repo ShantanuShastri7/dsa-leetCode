32825552c9204a9682967d4e913b577f8d603fc1
Shantanu Shastri <shantanushastri70@gmail.com>
2026-07-30 21:10:55 -0400

    Time: 1 ms (96.84%) | Memory: 42.5 MB (85.60%) - LeetSync

---
+++ DS/Math/4364-maximum-value-of-an-alternating-sequence/maximum-value-of-an-alternating-sequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long maximumValue(int n, int s, int m) {
+        if (n == 1) {
+            return s;
+        }
+
+        long k = n / 2;
+        
+        return s + (k * m) - (k - 1);
+    }
+}
